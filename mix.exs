fe7d78880e41dc946fc9e2e3a0a133f3b380a39a
Ivan Youroff <ivan.youroff@gmail.com>
2021-10-19 20:35:30 -0700

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Monex.Mixfile do
 
   def project do
     [app: :monex,
-     version: "0.1.16",
+     version: "0.1.17",
      elixir: "~> 1.4",
      description: description(),
      package: package(),
